f4931af0b0be3bf7bc3a7bae3258abf2f0db9d22
viktordanchenko <meteor2433@gmail.com>
2021-02-28 21:10:20 +0300

    work with dataset

--- FileUtils.py
+++ FileUtils.py
@@ -1,29 +1,29 @@
-import os
+from os.path import join
 
 from docx2python import docx2python
 from os import walk
 
-import Precondition
+from Precondition import clear_text_dataset
 
 PATH_DATASET = 'information system/dataset/'
 _, authors, _ = next(walk(PATH_DATASET))
 print(authors)
 
-
 d = dict()
 d.fromkeys(authors)
 
 for author in authors:
-    path_to_works_of_the_author = os.path.join(PATH_DATASET, author)
+    path_to_works_of_the_author = join(PATH_DATASET, author)
     _, _, filenames = next(walk(path_to_works_of_the_author))
-    print(filenames)
 
+    print(author)
+    print(filenames)
     global_vacab = set()
     for filename in filenames:
-        path = os.path.join(path_to_works_of_the_author, filename)
+        path = join(path_to_works_of_the_author, filename)
         # extract docx content
         doc_result = docx2python(path).text
-        text = Precondition.clear_text_dataset(doc_result)
+        text = clear_text_dataset(doc_result)
         if len(global_vacab) == 0:
             d.update({author: global_vacab.union(text)})
         else:
