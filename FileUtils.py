a091a37eb4de9840cabc27d059fd6efd9915ec8e
viktordanchenko <meteor2433@gmail.com>
2021-04-21 12:51:19 +0300

    added method split_text_on_vectors

--- FileUtils.py
+++ FileUtils.py
@@ -1,14 +1,19 @@
+import enum
+import os
 from os.path import join
+from typing import Dict
 
 from docx2python import docx2python
 from os import walk
 
-from Precondition import clear_text_dataset
+from Precondition import dataset_preprocessing, save_dataset_to_csv, split_text_on_vectors
 
 PATH_DATASET = 'information system/dataset/'
+BATCH_SIZE = 1024
+SEED = 123
 
 
-def get_dict_with_dataset():
+def get_all_dataset():
     print("DATASET")
     global authors, writings
     try:
@@ -25,14 +30,43 @@ def get_dict_with_dataset():
         except Warning:
             print(author, "'s dataset is empty")
         print("\n")
-        print(reformat_names(reformat_name(author)), "writing: ", sep="")
+        print(reformat_names(reformat_name(author)), "writing: ", sep=" ")
+        [print(reformat_names(writing), sep='\n') for writing in writings]
+        global_vacab = ''
+        for filename in writings:
+            path = join(path_to_works_of_the_author, filename)
+            # extract docx content
+            doc_result = docx2python(path).text
+            text = dataset_preprocessing(doc_result)
+            d.update({author: global_vacab.join(text)})
+    return d
+
+
+def get_df_from_dataset():
+    print("DATASET")
+    global authors, writings
+    try:
+        _, authors, _ = next(walk(PATH_DATASET))
+    except Warning:
+        print("Dataset is empty")
+    d = dict()
+    d.fromkeys(authors)
+
+    for author in authors:
+        path_to_works_of_the_author = join(PATH_DATASET, author)
+        try:
+            _, _, writings = next(walk(path_to_works_of_the_author))
+        except Warning:
+            print(author, "'s dataset is empty")
+        print("\n")
+        print(reformat_names(reformat_name(author)), "writing: ", sep=" ")
         [print(reformat_names(writing), sep='\n') for writing in writings]
         global_vacab = set()
         for filename in writings:
             path = join(path_to_works_of_the_author, filename)
             # extract docx content
             doc_result = docx2python(path).text
-            text = clear_text_dataset(doc_result)
+            text = dataset_preprocessing(doc_result)
             d.update({author: global_vacab.union(text)})
     return d
 
@@ -47,3 +81,24 @@ def reformat_names(name):
 def reformat_name(author):
     author_spit = reformat_names(author).split(" ", 1)
     return "{0}'s {1}".format(author_spit[0], author_spit[1])
+
+
+def csv_dataset():
+    if os.path.exists('information system/dataset/dataset.csv'):
+        os.remove("information system/dataset/dataset.csv")
+        print(f'\nOld dataset is removed\n')
+    result = f"\nDataset is saved as CSV file"
+    dict = get_all_dataset()
+    for author in Author:
+        try:
+            vectors = split_text_on_vectors(dict.get(author.name), 2)
+            save_dataset_to_csv(vectors, author.value)
+        except Exception:
+            return f"\nFail saving dataset on csv"
+    return result
+
+class Author(enum.Enum):
+    Orwell_George = "0"
+    Tolstoy_Lev_Nikolayevich = "1"
+    Zamyatin_Evgeny_Ivanovich = "2"
+    Pushkin_Alexander_Sergeevich = "3"
--- Main.py
+++ Main.py
@@ -1,5 +1,59 @@
-from FileUtils import get_dict_with_dataset
+import gensim
+import pandas as pd
+import numpy as np
+import tensorflow as tf
 
-dict = get_dict_with_dataset()
+from FileUtils import csv_dataset, Author
+from keras.models import Sequential
+from keras import layers
+from keras.optimizers import RMSprop,Adam
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras import regularizers
+from keras import backend as K
+from keras.callbacks import ModelCheckpoint
 
-print(dict.keys())
+#print(csv_dataset())
+
+train = pd.read_csv('information system/dataset/dataset.csv', sep=',')
+
+print(train.head(15))
+print(train.groupby('key').nunique())
+
+train = train[['sentence','key']]
+#print(train["sentence"].isnull().sum())
+
+labels = np.array(train['key'])
+y = []
+for i in range(len(labels)):
+    if labels[i] == Author.Orwell_George.value:
+        y.append(0)
+    if labels[i] == Author.Tolstoy_Lev_Nikolayevich.value:
+        y.append(1)
+    if labels[i] == Author.Zamyatin_Evgeny_Ivanovich.value:
+        y.append(2)
+    if labels[i] == Author.Pushkin_Alexander_Sergeevich.value:
+        y.append(3)
+y = np.array(y)
+labels = tf.keras.utils.to_categorical(y, 4, dtype="int")
+del y
+
+#print(labels)
+
+max_len = 200
+
+def sent_to_words(sentences):
+    for sentence in sentences:
+        yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))  # deacc=True removes punctuations
+
+
+sequences = np.array(train['sentence'])
+print(list(sent_to_words(sequences))[:20])
+
+
+
+#tweets = pad_sequences(sequences, maxlen=max_len)
+#print(tweets)
+#print(dict.get('Zamyatin_Evgeny_Ivanovich'))
+#print(dict_all_dataset.get('Zamyatin_Evgeny_Ivanovich'))
+#get_statistic_words_from_text(get_tokens_from_text(dict_all_dataset.get('Zamyatin_Evgeny_Ivanovich')))
